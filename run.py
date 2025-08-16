07cccd539d47b7c93dbed946f83c9a38fc7ed1d0
athenadev3 <athenadev3@athenamedia.online>
2025-08-16 10:15:05 +0100

    Added question list.

--- run.py
+++ run.py
@@ -7,20 +7,26 @@ class MB_Question():
         return "Question: " + self.question + "\nAnswer: " + self.answer
 
 
+main_question_list = []
+
 q1 = MB_Question()
 q1.question = "What is 13 + 7 ?"
 q1.answer = "20"
 
-print(q1)
+main_question_list.append(q1)
 
 q2 = MB_Question()
 q2.question = "You have 5 cakes and you give 2 to your friend.  How many do you have left?"
 q2.answer = "2"
 
-print(q2)
+main_question_list.append(q2)
+
 
 q3 = MB_Question()
 q3.question = "What is the area of a square that has 2cm sides?"
 q3.answer = "4cm\u00b2"
 
-print(q3)
+main_question_list.append(q3)
+
+for q in main_question_list:
+    print(q)
