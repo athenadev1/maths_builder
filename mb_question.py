9517e29a32dd3befd91bbf2674cd423dc27097af
athenadev3 <athenadev3@athenamedia.online>
2025-08-16 10:54:31 +0100

    moved mb_question into its own class file

---
+++ mb_question.py
@@ -0,0 +1,7 @@
+class MB_Question():
+    def __init__(self):
+        self.question = ""
+        self.answer = ""
+
+    def __repr__(self):
+        return "Question: " + self.question + "\nAnswer: " + self.answer
--- run.py
+++ run.py
@@ -1,10 +1,4 @@
-class MB_Question():
-    def __init__(self):
-        self.question = ""
-        self.answer = ""
-
-    def __repr__(self):
-        return "Question: " + self.question + "\nAnswer: " + self.answer
+from mb_question import MB_Question
 
 
 main_question_list = []
